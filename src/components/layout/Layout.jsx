d8559b6641997b4b7b6745e2eb2bf54427802a1c
anar123123 <arashidzade1991@gmail.com>
2025-05-19 04:35:13 +0400

    mibs

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router";
 import Header from "../../components/layout/Header";
 import Footer from "../../components/layout/Footer";
+import ScrollToTop from "../../ScrollToTop";
 
 export default function Layout(){
     return(
         <div>
+            <ScrollToTop />
             <Header />
             <Outlet />
             <Footer />
